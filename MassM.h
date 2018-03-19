2edf95b2a7b8ebd8181cf5681f57ebc1d0ff84e7
AndradeL <l182371@g.unicamp.br>
2018-03-19 16:21:02 +0000

    Add Mass matrix 1D

--- MassM.h
+++ MassM.h
@@ -8,35 +8,81 @@
 */
 class BMass1D : public BMoment1D
 {
-	int Mq;
-	int Mn;
+	int q;
+	int n;
+	int lenMass;
 	double **Matrix;
+	int *BinomialMat;
 
-	void create_matrix();
-public:
-	BMass1D() : BMoment1D()
-	{	}
+	// alloc matrix linearly
+	double** create_matrix();
 
-	BMass1D (int q, int n) : BMoment1D (q, 2 * n)
-	{	}
+	void compute_binomials();
+
+	// Usual factorial coefficient
+	int factorial (int n);
 	
-	~BMass1D();
+	// Overloaded factorial coefficient = factorial(n) / factorial(b),
+	// used for computing binomial coefficient
+	int factorial (int a, int b);
+
+	// Usual binomial coefficient
+	double binomial (int a, int b);
+
+public:
+	BMass1D (int q, int n);
+	
+	~BMass1D ();
+
+	// return the mass matrix values at indexes i and j
+	double getMatrixValue(int i, int j) { return Matrix[i][j]; }
+
+	// compute the mass matrix
+	void compute_matrix();
+
+	// sets the function definition or value before computing the mass matrix
+	void compute_matrix (double (*f) (double));
+	void compute_matrix (double *Fval);
 };
 
 class BMass2DTri : public BMoment2DTri
 {
-public:
-    // check about the 'q' value
-    BMass2DTri(int q, int n) : BMoment2DTri(q, 2 * n)
-    {	}
+	int q;
+	int n;
+	double **Matrix;
 
-    BMass2DTri(int q, int n, double T[][2]) : BMoment2DTri(q, 2 * n, T)
-	{	}
+public:
+    BMass2DTri (int q, int n);
+
+    BMass2DTri (int q, int n, double T[][2]);
+
+	~BMass2DTri ();
+
+	void compute_matrix ();
+
+	void compute_matrix (double (*f) (double));
+
+	void compute_matrix (double *Fval);
 };
 
 class BMass2DQuad : public BMoment2DQuad
 {
+	int q;
+	int n;
+	double **Matrix;
 
+public:
+	BMass2DQuad (int q, int n);
+
+    BMass2DQuad (int q, int n, double T[][2]);
+
+	~BMass2DQuad ();
+
+	void compute_matrix ();
+
+	void compute_matrix (double (*f) (double));
+
+	void compute_matrix (double *Fval);
 };
 
 class BMass3D : public BMoment3D
--- MassM1D.cpp
+++ MassM1D.cpp
@@ -1,29 +1,84 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
 #include "MassM.h"
 
-class BMass1D : public BMoment1D
+// maybe trade q to 2*q in the base class constructor
+BMass1D::BMass1D (int q, int n) : BMoment1D (q, 2 * n)
 {
-	int Mq;
-	int Mn;
-	double **Matrix;
+    this->q = q;
+    this->n = n;
 
-	void create_matrix()
-    {
-        double *aux = new double[(n+1) * (n+1)];
-        Matrix = new double* [n+1];
-        for (int i = 0; i <= n; aux += (n+1), i++)
-            Matrix[i] = aux;
-    }
-public:
-	BMass1D (int q, int n) : BMoment1D (q, 2 * n)
-	{
+    lenMass = n+1;
 
-    }
+    Matrix = create_matrix();
+}
 	
-	~BMass1D()
-	{
-        delete Matrix[0];
-        delete Matrix;
+BMass1D::~BMass1D ()
+{
+    delete Matrix[0];
+    delete Matrix;
+}
+
+double** BMass1D::create_matrix ()
+{
+    double *aux = new double [lenMass * lenMass];
+    double **matrix = new double* [lenMass];
+    for (int i = 0; i < lenMass; aux += lenMass, i++)
+        matrix[i] = aux;
+    return matrix;
+}
+
+void BMass1D::compute_binomials ()
+{
+
+}
+
+int BMass1D::factorial (int n)
+{
+    if (n > 1)
+        return n * factorial(n-1);
+    else
+        return 1;
+}
+
+int BMass1D::factorial (int n, int b)
+{
+	if (n > b)
+		return n * factorial(n-1, b);
+	else
+		return 1;
+}
+
+double BMass1D::binomial (int a, int b)
+{
+	return (double)factorial(a, b) / factorial(a - b);
+}
+
+void BMass1D::compute_matrix ()
+{
+    compute_moments();
+    //compute_binomials();
+
+    for (int i = 0; i < lenMass; i++)
+    {
+        for (int j = 0; j < lenMass; j++)
+        {
+            Matrix[i][j] = (binomial(i+j, i) / binomial(2*n, n)) * binomial(2*n - (i+j), n - i) * get_bmoment(i);
+        }
     }
+}
 
+void BMass1D::compute_matrix (double (*f) (double))
+{
+    setFunctionDef(f);
+    compute_matrix();
+}
 
-};
+void BMass1D::compute_matrix (double *Fval)
+{
+    setFunctionValue(Fval);
+    compute_matrix();
+}
