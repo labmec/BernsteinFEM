d48e89e062c6d44267b9a001a12d594759aa55e0
AndradeL <l182371@g.unicamp.br>
2018-03-22 17:47:38 +0000

    Add 1-dimensional stiffness matrix

--- StiffM.h
+++ StiffM.h
@@ -3,41 +3,89 @@
 #ifndef STIFFM_H
 #define STIFFM_H
 
-/*to implement yet
-  About computing the Stiffness matrix
-*/
+/*****************************************************************************
+ * Bernstein Stiffness Matrix for 1-dimensional elements                     *
+ *****************************************************************************/
 class BStiff1D : public BMoment1D
 {
+    int q;              // number of quadrature points ( recommended: 2*(n+1) )
+    int n;              // polynomial order
+    int lenStiff;       // length of the stiffness matrix
+    double **Matrix;    // stiffness matrix
+    int lenBinomialMat; // length of the binomial Pascal matrix
+    int **BinomialMat;  // Pascal Matrix
 
+    // alloc matrix linearly
+    double **create_matrix();
+
+    void delete_matrix(double **matrix);
+
+    // alloc binomial matrix
+    int **create_binomialMat();
+
+    void delete_binomialMat(int **binomialMat);
+
+    // computes a Pascal Matrix with size lenBinomialMat
+    void compute_binomials();
+
+    // computes the normalized moment
+    static double normalize(int k, int l, double moment)
+    {
+        // gradient is always 1 or -1 in the 1-dimensional case
+        if (k == l)
+            return moment;
+        else
+            return moment * (-1);
+    }
+
+  public:
+    BStiff1D(int q, int n);
+
+    ~BStiff1D();
+
+    void compute_matrix();
+
+    void compute_matrix(double *Fval);
+
+    void compute_matrix(double (*f)(double));
 };
 
+/*****************************************************************************
+ * Bernstein Stiffness Matrix for triangular elements (2-dimensional)        *
+ *****************************************************************************/
 class BStiff2DTri : public BMoment2DTri
 {
-    int qS;
-    int nS;
-public:
-    // check about the 'q' value
-    BStiff2DTri (int q, int n) : BMoment2DTri (q, 2*n - 2)
-    {
-        qS = q;
-        nS = n;
-    }
+    int q;              // number of quadrature points ( recommended: 2*(n+1) )
+    int n;              // polynomial order
+    int lenStiff;       // length of the stiffness matrix
+    double **Matrix;    // stiffness matrix
+    int lenBinomialMat; // length of the binomial Pascal matrix
+    int **BinomialMat;  // Pascal Matrix
 
-    BStiff2DTri (int q, int n, double T[][2]) : BMoment2DTri (q, 2*n - 2, T)
-    {    
-        qS = q;
-        nS = n;
-    }
+  public:
+    BStiff2DTri(int q, int n);
+
+    BStiff2DTri(int q, int n, double T[][2]);
 };
 
+/*****************************************************************************
+ * Bernstein Stiffness Matrix for quadrilateral elements (2-dimensional)     *
+ *****************************************************************************/
 class BStiff2DQuad : public BMoment2DQuad
 {
+    int q;              // number of quadrature points ( recommended: 2*(n+1) )
+    int n;              // polynomial order
+    int lenStiff;       // length of the stiffness matrix
+    double **Matrix;    // stiffness matrix
+    int lenBinomialMat; // length of the binomial Pascal matrix
+    int **BinomialMat;  // Pascal Matrix
 
+  public:
+    BStiff2DQuad(int q, int n);
 };
 
 class BStiff3D : public BMoment3D
 {
-
 };
 
 #endif
---
+++ StiffM1D.cpp
@@ -0,0 +1,106 @@
+#include "StiffM.h"
+
+BStiff1D::BStiff1D(int q, int n) : BMoment1D(q, 2 * (n - 1))
+{
+    this->q = q;
+    this->n = n;
+
+    lenStiff = n + 1;
+    lenBinomialMat = n + 1;
+
+    Matrix = create_matrix();
+    BinomialMat = create_binomialMat();
+}
+
+BStiff1D::~BStiff1D()
+{
+    delete_matrix(Matrix);
+    delete_binomialMat(BinomialMat);
+}
+
+double **BStiff1D::create_matrix()
+{
+    double *aux = new double[lenStiff * lenStiff];
+    double **matrix = new double *[lenStiff];
+
+    for (int i = 0; i < lenStiff; aux += lenStiff, i++)
+        matrix[i] = aux;
+
+    return matrix;
+}
+
+void BStiff1D::delete_matrix(double **matrix)
+{
+    delete matrix[0];
+    delete matrix;
+}
+
+int **BStiff1D::create_binomialMat()
+{
+    int *aux = new int[lenBinomialMat * lenBinomialMat];
+    int **matrix = new int *[lenBinomialMat];
+
+    for (int i = 0; i < lenBinomialMat; aux += lenBinomialMat, i++)
+        matrix[i] = aux;
+
+    return matrix;
+}
+
+void BStiff1D::delete_binomialMat(int **binomialMat)
+{
+    delete binomialMat[0];
+    delete binomialMat;
+}
+
+void BStiff1D::compute_binomials()
+{
+    for (int i = 0; i < lenBinomialMat; i++)
+    {
+        for (int j = 0; j < lenBinomialMat; j++)
+        {
+            BinomialMat[i][j] = 0;
+        }
+    }
+    for (int i = 0; i < lenBinomialMat; i++)
+        BinomialMat[i][0] += 1;
+
+    for (int j = 1; j < lenBinomialMat; j++)
+        BinomialMat[0][j] += 1;
+
+    for (int k = 1; k < lenBinomialMat; k++)
+    {
+        for (int l = 1; l < lenBinomialMat; l++)
+        {
+            BinomialMat[k][l] += BinomialMat[k][l - 1] + BinomialMat[k - 1][l];
+        }
+    }
+}
+
+void BStiff1D::compute_matrix()
+{
+    double Const = 1 / BinomialMat[n - 1][n - 1];
+    for (int i = 0; i < lenStiff - 1; i++)
+    {
+        for (int j = 0; j < lenStiff - 1; j++)
+        {
+            double w = BinomialMat[i][j] * Const;
+            w *= BinomialMat[n - i - 1][n - j - 1];
+
+            for (int k = 1; k <= 2; k++)
+                for (int l = 1; l <= 2; l++)
+                    Matrix[i][j] += w * normalize(k, l, get_bmoment(i + j));
+        }
+    }
+}
+
+void BStiff1D::compute_matrix(double *Fval)
+{
+    setFunctionValue(Fval);
+    compute_matrix();
+}
+
+void BStiff1D::compute_matrix(double (*f)(double))
+{
+    setFunctionDef(f);
+    compute_matrix();
+}
