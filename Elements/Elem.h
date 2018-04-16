88cdac8d58bf9a73806e906d956e37ad052e6a20
AndradeL <l182371@g.unicamp.br>
2018-04-16 08:37:16 -0300

    Format Elem.h file

--- Elements/Elem.h
+++ Elements/Elem.h
@@ -6,19 +6,19 @@
 
 class BElement1D
 {
-  int q;                // number of element quadrature points
-  int n;                // element polynomial basis order
-  int length;           // number of element points
-  double *BBVector;     // Bernstein-Bézier polynomial basis coefficients
-  double *QuadVector;   // BB polynomial evaluated at quadrature points
-  double *MassFval;     // mass matrix function values -- stil think it might be unecessary
+  int q;              // number of element quadrature points
+  int n;              // element polynomial basis order
+  int length;         // number of element points
+  double *BBVector;   // Bernstein-Bézier polynomial basis coefficients
+  double *QuadVector; // BB polynomial evaluated at quadrature points
+  double *MassFval;   // mass matrix function values -- stil think it might be unecessary
   //double *ConvecFval;   // convective matrix function values
-  double *StiffFval;    // stiffness matrix values
-  BMass1D MassMat;     // mass matrix object
+  double *StiffFval;  // stiffness matrix values
+  BMass1D MassMat;    // mass matrix object
   //BConvec1D *ConvecMat; // convective matrix object
-  BStiff1D StiffMat;   // stiffness matrix object
-  double **ElMat;       // element matrix = mass + convec + stiff
-  BMoment1D LoadVec;   // load vector in moments object
+  BStiff1D StiffMat;  // stiffness matrix object
+  double **ElMat;     // element matrix = mass + convec + stiff
+  BMoment1D LoadVec;  // load vector in moments object
 
   double **create_el_mat();
 
@@ -38,26 +38,48 @@ public:
 
   int len() { return length; }
 
-  double getMatrixValue (int i, int j) { if (i < length && j < length) return ElMat[i][j]; else return 0.0;}
+  double getMatrixValue(int i, int j)
+  {
+    if (i < length && j < length)
+      return ElMat[i][j];
+    else
+      return 0.0;
+  }
 
-  double getLoadVector (int i) { if (i < length) return LoadVec.get_bmoment(i); else return 0.0; }
+  double getLoadVector(int i)
+  {
+    if (i < length)
+      return LoadVec.get_bmoment(i);
+    else
+      return 0.0;
+  }
 
-  double getQuadVector(int i) { if (i < q) return QuadVector[i]; else return 0.0; }
+  double getQuadVector(int i)
+  {
+    if (i < q)
+      return QuadVector[i];
+    else
+      return 0.0;
+  }
 
   // sets element interval
-  void setInterval (double a, double b);
+  void setInterval(double a, double b);
 
   // sets the BBVector
-  void setBBVector (double* vec) { for (int i = 0; i < length; i++) BBVector[i] = vec[i]; }
+  void setBBVector(double *vec)
+  {
+    for (int i = 0; i < length; i++)
+      BBVector[i] = vec[i];
+  }
 
   // sets functions by function value
-  void setMassFunction (double* Fval) { MassMat.setFunction(Fval); }
+  void setMassFunction(double *Fval) { MassMat.setFunction(Fval); }
   //void setConvecFunction (double* Fval);
-  void setStiffFunction (double* Fval) {StiffMat.setFunction(Fval); }
-  void setLoadFunction (double* Fval) { LoadVec.setFunction(Fval); }
-  
+  void setStiffFunction(double *Fval) { StiffMat.setFunction(Fval); }
+  void setLoadFunction(double *Fval) { LoadVec.setFunction(Fval); }
+
   // evaluate the BBVector at the quadrature points
-  double* evaluate();
+  double *evaluate();
 
   // computes all matrices and stores the sum into ElMat
   void makeSystem();
@@ -65,19 +87,19 @@ public:
 
 class BElement2DTri
 {
-  int q;                   // number of element quadrature points
-  int n;                   // element polynomial basis order
-  int length;              // number of element points
-  double *BBVector;        // Bernstein-Bézier polynomial basis coefficients
-  double *QuadVector;      // BB polynomial evaluated at quadrature points
-  double *MassFval;        // mass matrix function values
+  int q;                // number of element quadrature points
+  int n;                // element polynomial basis order
+  int length;           // number of element points
+  double *BBVector;     // Bernstein-Bézier polynomial basis coefficients
+  double *QuadVector;   // BB polynomial evaluated at quadrature points
+  double *MassFval;     // mass matrix function values
   //double *ConvecFval;      // convective matrix function values
-  double *StiffFval;       // stiffness matrix values
-  BMass2DTri MassMat;     // mass matrix object
+  double *StiffFval;    // stiffness matrix values
+  BMass2DTri MassMat;   // mass matrix object
   //BConvec2DTri *ConvecMat; // convective matrix object
-  BStiff2DTri StiffMat;   // stiffness matrix object
-  double **ElMat;          // element matrix = mass + convec + stiff
-  BMoment2DTri LoadVec;   // load vector in moments object
+  BStiff2DTri StiffMat; // stiffness matrix object
+  double **ElMat;       // element matrix = mass + convec + stiff
+  BMoment2DTri LoadVec; // load vector in moments object
 
   double **create_el_mat();
 
@@ -92,28 +114,50 @@ public:
 
   int len() { return length; }
 
-  double getMatrixValue (int i, int j) { if (i < length && j < length) return ElMat[i][j]; else return 0.0;}
+  double getMatrixValue(int i, int j)
+  {
+    if (i < length && j < length)
+      return ElMat[i][j];
+    else
+      return 0.0;
+  }
 
-  double getLoadVector(int i) { if (i < length) return LoadVec.get_bmoment(i); else return 0.0; }
+  double getLoadVector(int i)
+  {
+    if (i < length)
+      return LoadVec.get_bmoment(i);
+    else
+      return 0.0;
+  }
 
   double getLoadVector(int a1, int a2) { return LoadVec.get_bmoment(a1, a2); }
 
-  double getQuadVector(int i) { if (i < q) return QuadVector[i]; else return 0.0;}
+  double getQuadVector(int i)
+  {
+    if (i < q)
+      return QuadVector[i];
+    else
+      return 0.0;
+  }
 
   // sets element triangle
-  void setTriangle (double v1[2], double v2[2], double v3[2]);
+  void setTriangle(double v1[2], double v2[2], double v3[2]);
 
   // sets the BBVector
-  void setBBVector (double* vec) { for (int i = 0; i < length; i++) BBVector[i] = vec[i]; }
+  void setBBVector(double *vec)
+  {
+    for (int i = 0; i < length; i++)
+      BBVector[i] = vec[i];
+  }
 
   // sets functions by function value
-  void setMassFunction (double* Fval) { MassMat.setFunction(Fval); }
+  void setMassFunction(double *Fval) { MassMat.setFunction(Fval); }
   //void setConvecFunction (double* Fval);
-  void setStiffFunction (double* Fval) {StiffMat.setFunction(Fval); }
-  void setLoadFunction (double* Fval) { LoadVec.setFunction(Fval); }
+  void setStiffFunction(double *Fval) { StiffMat.setFunction(Fval); }
+  void setLoadFunction(double *Fval) { LoadVec.setFunction(Fval); }
 
   // evaluate the BBVector at the quadrature points
-  double* evaluate();
+  double *evaluate();
 
   // computes all matrices and stores the sum into ElMat
   void makeSystem();
@@ -121,19 +165,19 @@ public:
 
 class BElement2DQuad
 {
-  int q;                    // number of element quadrature points
-  int n;                    // element polynomial basis order
-  int length;               // number of element points
-  double *BBVector;         // Bernstein-Bézier polynomial basis coefficients
-  double *QuadVector;       // BB polynomial evaluated at quadrature points
-  double *MassFval;         // mass matrix function values
+  int q;                 // number of element quadrature points
+  int n;                 // element polynomial basis order
+  int length;            // number of element points
+  double *BBVector;      // Bernstein-Bézier polynomial basis coefficients
+  double *QuadVector;    // BB polynomial evaluated at quadrature points
+  double *MassFval;      // mass matrix function values
   //double *ConvecFval;       // convective matrix function values
-  double *StiffFval;        // stiffness matrix values
-  BMass2DQuad MassMat;     // mass matrix object
+  double *StiffFval;     // stiffness matrix values
+  BMass2DQuad MassMat;   // mass matrix object
   //BConvec2DQuad *ConvecMat; // convective matrix object
-  BStiff2DQuad StiffMat;   // stiffness matrix object
-  double **ElMat;           // element matrix = mass + convec + stiff
-  BMoment2DQuad LoadVec;   // load vector in moments object
+  BStiff2DQuad StiffMat; // stiffness matrix object
+  double **ElMat;        // element matrix = mass + convec + stiff
+  BMoment2DQuad LoadVec; // load vector in moments object
 
   double **create_el_mat();
 
@@ -146,28 +190,50 @@ public:
 
   //double** getElementMatrix() { return ElMat; }
 
-  double getMatrixValue (int i, int j) { if (i < length && j < length) return ElMat[i][j]; else return 0.0;}
+  double getMatrixValue(int i, int j)
+  {
+    if (i < length && j < length)
+      return ElMat[i][j];
+    else
+      return 0.0;
+  }
 
-  double getLoadVector(int i) { if (i < length) return LoadVec.get_bmoment(i); else return 0.0; }
+  double getLoadVector(int i)
+  {
+    if (i < length)
+      return LoadVec.get_bmoment(i);
+    else
+      return 0.0;
+  }
 
   double getLoadVector(int a1, int a2) { return LoadVec.get_bmoment(a1, a2); }
 
-  double getQuadVector(int i) { if (i < q) return QuadVector[i]; else return 0.0;}
+  double getQuadVector(int i)
+  {
+    if (i < q)
+      return QuadVector[i];
+    else
+      return 0.0;
+  }
 
   // sets element quadrilateral
-  void setQuad (double v1[2], double v2[2], double v3[2], double v4[2]);
+  void setQuad(double v1[2], double v2[2], double v3[2], double v4[2]);
 
   // sets the BBVector
-  void setBBVector (double* vec) { for (int i = 0; i < length; i++) BBVector[i] = vec[i]; }
+  void setBBVector(double *vec)
+  {
+    for (int i = 0; i < length; i++)
+      BBVector[i] = vec[i];
+  }
 
   // sets functions by function value
-  void setMassFunction (double* Fval) { MassMat.setFunction(Fval); }
+  void setMassFunction(double *Fval) { MassMat.setFunction(Fval); }
   //void setConvecFunction (double* Fval);
-  void setStiffFunction (double* Fval) {StiffMat.setFunction(Fval); }
-  void setLoadFunction (double* Fval) { LoadVec.setFunction(Fval); }
-  
+  void setStiffFunction(double *Fval) { StiffMat.setFunction(Fval); }
+  void setLoadFunction(double *Fval) { LoadVec.setFunction(Fval); }
+
   // evaluate the BBVector at the quadrature points
-  double* evaluate();
+  double *evaluate();
 
   // computes all matrices and stores the sum into ElMat
   void makeSystem();
