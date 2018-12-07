5388665581210b3408e447c9c5bd970a3841515d
AndradeL <l182371@g.unicamp.br>
2018-12-07 21:21:21 +0000

    Format file

--- Elements/LinearEl.cpp
+++ Elements/LinearEl.cpp
@@ -14,31 +14,34 @@ Element<LinearEl>::Element()
 
 template <>
 Element<LinearEl>::Element(const arma::mat &v)
-    : vertices(v), coordinates(1, 1, arma::fill::none)
+    : vertices(2, 1, arma::fill::none),
+      coordinates(1, 1, arma::fill::none)
 {
-    if (vertices.size() < 2)
+    if (vertices.n_rows < 2)
     {
-        throw std::logic_error("Invalid constructor argument, LinearEl vertices should have at least size 2");
+        throw std::invalid_argument("Invalid constructor argument, LinearEl vertices should have at least size 2");
+    }
+    else
+    {
+        vertices(0) = v(0);
+        vertices(1) = v(1);
     }
 }
 
 template <>
 Element<LinearEl>::Element(const Element<LinearEl> &cp)
-    : vertices(2, 1, arma::fill::none),
-      coordinates(1, 1, arma::fill::none)
-{
-    vertices(0) = cp.vertices(0);
-    vertices(1) = cp.vertices(1);
-}
+    : vertices(cp.vertices),
+      coordinates(1, 1, arma::fill::none) {}
 
 template <>
 const arma::mat &Element<LinearEl>::mapToElement(const arma::mat &coordinates, arma::mat &jacobian)
 {
-    try {
+    try
+    {
         this->coordinates(0) = (coordinates(0) + vertices(0)) * (vertices(1) - vertices(0));
         jacobian(0) = vertices(1) - vertices(0);
     }
-    catch(std::logic_error e)
+    catch (std::logic_error &e)
     {
         std::cerr << e.what() << std::endl;
         std::cerr << "invalid arguments (probably the size) in 'mapToElement' method call" << std::endl;
