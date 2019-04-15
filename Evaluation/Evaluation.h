f7af1937cad09318d73306e94f7b6cfaa9023b63
AndradeL <l182371@g.unicamp.br>
2019-04-15 18:43:41 +0000

    Header from last commit was missing

--- Evaluation/Evaluation.h
+++ Evaluation/Evaluation.h
@@ -8,12 +8,13 @@ template <Element_t EL>
 class BEval
 {
   protected:
-    uint q;               // number of integration points
-    uint n;               // polynomial order
-    arma::vec BBVec;     // Vector holding coefficients
-    uint bbvec_len;      // length BBVec should have
-    arma::vec eval;      // vector holding the polynomial evaluated in the integration points of the element
-    Element<EL> element; // element of computation
+    uint q;                 // number of integration points
+    uint n;                 // polynomial order
+    arma::vec BBVec;        // Vector holding coefficients
+    uint bbvec_len;         // length BBVec should have
+    arma::vec eval;         // vector holding the polynomial evaluated in the integration points of the element
+    Element<EL> element;    // element of computation
+    bool evaluated = false; // indicates whether evaluation was already evaluated or not
 
   public:
     BEval(uint q, uint n, Element<EL> const &element = Element<EL>());
@@ -36,6 +37,9 @@ class BEval
 
     void setCoefficientsVec(arma::vec const &vec);
 
+    // sets this object's with values of 0
+    void Zero();
+
     // evaluates the resulting polynomial at the integration points
     // and returns the resulting vector
     virtual arma::vec &computeEvaluation() = 0;
@@ -51,6 +55,8 @@ class BEval1D : public BEval<Element_t::LinearEl>
     BEval1D(uint q, uint n, arma::vec const &coeffVec, Element<Element_t::LinearEl> const &element = Element<Element_t::LinearEl>());
 
     arma::vec &computeEvaluation();
+
+    double L2Norm();
 };
 
 class BEval2DTri : public BEval<Element_t::TriangularEl>
@@ -63,6 +69,8 @@ class BEval2DTri : public BEval<Element_t::TriangularEl>
     BEval2DTri(uint q, uint n, arma::vec const &coeffVec, Element<Element_t::TriangularEl> const &element = Element<Element_t::TriangularEl>());
 
     arma::vec &computeEvaluation();
+
+    double L2Norm();
 };
 
 class BEval2DQuad : public BEval<Element_t::QuadrilateralEl>
@@ -75,6 +83,8 @@ class BEval2DQuad : public BEval<Element_t::QuadrilateralEl>
     BEval2DQuad(uint q, uint n, arma::vec const &coeffVec, Element<Element_t::QuadrilateralEl> const &element = Element<Element_t::QuadrilateralEl>());
 
     arma::vec &computeEvaluation();
+
+    double L2Norm();
 };
 
 class BEval3DCube : public BEval<Element_t::CubeEl>
@@ -85,6 +95,8 @@ class BEval3DCube : public BEval<Element_t::CubeEl>
     BEval3DCube(uint q, uint n, arma::vec const &coeffVec, Element<Element_t::CubeEl> const &element = Element<Element_t::CubeEl>());
 
     arma::vec &computeEvaluation();
+
+    double L2Norm();
 };
 
 class BEval3DTetra : public BEval<Element_t::TetrahedronEl>
@@ -95,4 +107,6 @@ class BEval3DTetra : public BEval<Element_t::TetrahedronEl>
     BEval3DTetra(uint q, uint n, arma::vec const &coeffVec, const Element<Element_t::TetrahedronEl> &element = Element<Element_t::TetrahedronEl>());
 
     arma::vec &computeEvaluation();
+
+    double L2Norm();
 };
